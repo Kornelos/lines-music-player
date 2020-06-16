420fa41dd087b6d982fcac1d541d8fe7b052df18
Kornelos <kornel1902@gmail.com>
2020-06-16 20:18:59 +0200

    chmod and sh

--- prepare_wrt.sh
+++ prepare_wrt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 wget http://10.0.2.2:8000/kmod-drv-mpc8xxx_4.14.171-1_aarch64_generic.ipk
 opkg install kmod-drv-mpc8xxx_4.14.171-1_aarch64_generic.ipk
--- start_player.sh
+++ start_player.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 screen -m -d server.py
 cd songs
 screen -m -d python3 -m http.server
