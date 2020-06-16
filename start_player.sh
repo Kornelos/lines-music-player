23707b46f00d6d800c96f14605df8939d284d07e
Kornelos <kornel1902@gmail.com>
2020-06-16 22:14:15 +0200

    start

--- start_player.sh
+++ start_player.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-# run mpd
-screen -m -d mpd
 
 #probe gpiod
 modprobe gpio-mpc8xxx
-
-#run flask
-screen -m -d server.py
 cd songs
 screen -m -d python3 -m http.server
+cd ..
+#run flask
+screen -m -d server.py
